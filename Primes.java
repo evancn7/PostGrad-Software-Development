259b5cb00f8c77a4db1746468ac946d9bf52806b
Evan Campion <evancn7@gmail.com>
2020-10-24 13:13:27 +0100

    added comments to the code

--- Primes.java
+++ Primes.java
@@ -3,7 +3,6 @@ of prime numbers that exist in that range, inclusive of the specified boundaries
 For example, the quantity of prime numbers that exist between 7 and 19 is 5
 (namely 7, 11, 13, 17 and 19)*/
 import java.util.Scanner;
-
 public class Primes {
     public static void main(String args[] ) throws Exception {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT */
@@ -11,14 +10,17 @@ public class Primes {
         Scanner scan = new Scanner(System.in);
         int lower = scan.nextInt();
         int upper = scan.nextInt();
+        // swap the values if upper bound is entered first
         if (lower > upper){
             int temp;
             temp = lower;
             lower = upper;
             upper = temp;
         }
+        // declare array with the boundaries
         int[] array = new int[upper-lower+1];
         int pos = 0;
+        // fill array with values between the bounsaries skipping even numbers
         for (int i=lower; i<=upper; i++){
             if (i > 3 && i % 2 == 0){
                 continue;
