cafedd566555b37597f400b8226992ab1e9377c2
Evan Campion <evancn7@gmail.com>
2020-11-06 17:24:49 +0000

    Uncommented isCoPrime. Added more efficient findHighestCD function

--- MonteCarloCoPrime.java
+++ MonteCarloCoPrime.java
@@ -5,23 +5,32 @@ public class MonteCarloCoPrime{
 
   public static void main(String[] args){
     final long startTime = System.currentTimeMillis();
-    System.out.println(monteCarloLoop(7, 13, 10000));
+    System.out.println(monteCarloLoop(3, 11, 10000));
     final long endTime = System.currentTimeMillis();
     System.out.println("Execution time: "+((endTime - startTime) * 0.001));
   }
 
-  public static boolean isCoPrime(int a, int b){
-    // the greatest common divisor must be 1 to return true
-    boolean flag = true; // assume true
-    int limit = Math.min(a, b);
-    // 1 is always a common denominator so start i=2 and stop at lower divisor
-    for (int i=2; i<=limit; i++){
-      // if anything above i divides in then flag is flipped to false
-      if (a % i == 0 && b % i == 0){
-        flag = false; // false otherwise
-      }
+  // public static boolean isCoPrime(int a, int b){
+  //   // the greatest common divisor must be 1 to return true
+  //   boolean flag = true; // assume true
+  //   int limit = Math.min(a, b);
+  //   // 1 is always a common denominator so start i=2 and stop at lower divisor
+  //   for (int i=2; i<=limit; i++){
+  //     // if anything above i divides in then flag is flipped to false
+  //     if (a % i == 0 && b % i == 0){
+  //       flag = false; // false otherwise
+  //     }
+  //   }
+  //   return flag;
+  // }
+
+  public static int findHighestCD(int a, int b){
+    if (b != 0){
+      return findHighestCD(b, a % b);
     }
-    return flag;
+    else{
+      return a;
+    }
   }
 
   public static int monteCarloLoop(int divisor1, int divisor2, int numTrials){
