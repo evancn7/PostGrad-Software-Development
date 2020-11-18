6bc823521c0ba5d823c1c03bb2340f8d03c73821
Evan Campion <evancn7@gmail.com>
2020-11-18 21:43:17 +0000

    made a start on the intake of user commands

--- StackMain.java
+++ StackMain.java
@@ -1,6 +1,30 @@
-public static void main(String[] args){
-  Stack myStack = new Stack(5);
-  myStack.push(5);
-  myStack.push(7);
-  System.out.println(myStack.pop());
+import java.util.Scanner;
+public class StackMain {
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+
+    // take user input for the length of the stack
+    int stackLength = scan.nextInt();
+
+    // create userStack from user parameter stackLength
+    Stack userStack = new Stack(stackLength);
+
+    // clear buffer
+    scan.nextLine();
+
+    // create loop for taking in commands from user
+    for (int i=0; i<stackLength; i++){
+      String command = scan.nextLine();
+      String[] commandParts = command.split(" ");
+      String method = commandParts[0].toLowerCase();
+
+      // once command is entered then execute the command on the stack
+      if (method.compareTo("push") == 0){
+        userStack.push(Integer.parseInt(commandParts[1]));
+      }
+      else if (method.compareTo("pop") == 0){
+        userStack.pop();
+      }
+    }
+  }
 }
