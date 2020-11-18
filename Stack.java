999aa077b183b6cfebaf489bf8c363c8308b7b09
Evan Campion <evancn7@gmail.com>
2020-11-18 13:23:01 +0000

    duplicate commite due to rename of file

---
+++ Stack.java
@@ -0,0 +1,48 @@
+public class Stack {
+      // initialize the variables for the stack creation
+      private int maxSize;
+      private int[] stackArray;
+      private int top;
+    
+      // general constructor
+      public Stack(int s){
+        maxSize = s;
+        stackArray = new int[maxSize];
+        top = -1;
+      }
+      // push method to add items to top of stackArray
+      public void push(int item){
+        top++;
+        stackArray[top] = item;
+      }
+      // pop method to extract items from top of stackArray
+      public int pop(){
+        return stackArray[top--];
+      }
+      // method to peek at what is at the top of the stackArray
+      public int peek(){
+        return stackArray[top];
+      }
+      // method to check position of top
+      public int getTop(){
+        return this.top;
+      }
+      // method to check if stackArray is empty
+      public boolean isEmpty(){
+        return top == -1;
+      }
+      // method to check if stackArray is full
+      public boolean isFull(){
+        return top == maxSize-1;
+      }
+      // method to empty the stackArray
+      public void makeEmpty(){
+        top = -1;
+      }
+    public static void main(String[] args){
+      Stack myStack = new Stack(5);
+      myStack.push(5);
+      myStack.push(7);
+      System.out.println(myStack.pop());
+  }
+}
