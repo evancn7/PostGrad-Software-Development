4885a553e1391a0d006792121c33e544b57b0f71
Evan Campion <evancn7@gmail.com>
2020-11-19 00:37:39 +0000

    loop to find the highest number in stack and return it

--- StackMain.java
+++ StackMain.java
@@ -2,22 +2,17 @@ import java.util.Scanner;
 public class StackMain {
   public static void main(String[] args){
     Scanner scan = new Scanner(System.in);
-
     // take user input for the length of the stack
     int stackLength = scan.nextInt();
-
     // create userStack from user parameter stackLength
     Stack userStack = new Stack(stackLength);
-
     // clear buffer
     scan.nextLine();
-
     // create loop for taking in commands from user
     while (!userStack.isFull()){
       String command = scan.nextLine();
       String[] commandParts = command.split(" ");
       String method = commandParts[0].toLowerCase();
-
       // once command is entered then execute the command on the stack
       if (method.compareTo("push") == 0){
         userStack.push(Integer.parseInt(commandParts[1]));
@@ -26,9 +21,17 @@ public class StackMain {
         userStack.pop();
       }
     }
-    // print the stack
+    // create loop to find the highest number in the stack and return to user
+    // declare variable to keep track of highest number
+    int high = userStack.pop();
     while (!userStack.isEmpty()){
-      System.out.println(userStack.pop());
+      if (userStack.pop() > high){
+        high = userStack.pop();
+      }
+      else{
+        continue;
+      }
     }
+    System.out.println(high);
   }
 }
