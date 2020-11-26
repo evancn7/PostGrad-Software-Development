d0541e8e0c471abc32ccb1431f5b37ef673b13fe
Evan Campion <evancn7@gmail.com>
2020-11-26 14:02:20 +0000

    edit comments

--- QueueMain.java
+++ QueueMain.java
@@ -11,19 +11,22 @@ public class QueueMain{
     String s = "INSERT this\nINSERT is\nINSERT how\nINSERT to\nREMOVE\nINSERT do\nREMOVE\nINSERT it";
     Scanner scan = new Scanner(s);
     Queue myQ = new Queue(20);
-    // while the queue has next line, amount of commands not defined
+
+    // while the queue has next line, as amount of commands is not defined
     while (scan.hasNextLine()){
       String command = scan.nextLine();
       String[] commandSplit = command.split(" ");
-      // convert command to lower case for comperison
+      // convert command to lower case for comparison
       String method = commandSplit[0].toLowerCase();
       // once command is entered then execute the command on the queue
       // once determined that command is insert then take 2nd part of command
       if (method.compareTo("insert") == 0) myQ.insert(commandSplit[1]);
       else myQ.remove();
     }
+
+    // close resource
     scan.close();
-    // end of creating the queue from commandSplit
+
     // find the middle value, if even then find value nearest the front
     int middleItem;
     if (myQ.size() % 2 == 0) middleItem = myQ.size() / 2 - 1;
@@ -33,6 +36,7 @@ public class QueueMain{
     middleItem = middleItem + myQ.frontPosition();
     while (myQ.frontPosition() != middleItem) myQ.remove();
 
+    // print middle item to screen
     System.out.println(myQ.remove());
   }
 }
