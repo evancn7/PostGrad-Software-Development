d01ee59ac71ef14f107fc67b177dba731daf8aef
Evan Campion <evancn7@gmail.com>
2021-02-18 21:13:51 +0000

    completed huffman algorithm

--- HuffmanEncoding.java
+++ HuffmanEncoding.java
@@ -3,141 +3,121 @@ import java.util.*;
 public class HuffmanEncoding {
     public static void main(String args[]) throws Exception {
         Scanner in = new Scanner(System.in);
-        String sentence = in .nextLine();
+        String sentence = in.nextLine();
 
         int[] array = new int[256]; //an array to store all the frequencies
 
-        for (int i = 0; i < sentence.length(); i++) { //go through the sentence
-            array[(int) sentence.charAt(i)]++; //increment the appropriate frequencies
+        // go through the sentence
+        for (int i = 0; i < sentence.length(); i++) {
+            //increment the appropriate frequencies
+            array[(int) sentence.charAt(i)]++;
         }
 
-        PriorityQueue < Tree > PQ = new PriorityQueue < Tree > (); //make a priority queue to hold the forest of trees
+        // make a priority queue to hold the forest of trees
+        PriorityQueue <Tree> PQ = new PriorityQueue <Tree>();
 
-        for (int i = 0; i < array.length; i++) { //go through frequency array
-            if (array[i] > 0) { //print out non-zero frequencies - cast to a char
+        // go through frequency array
+        for (int i = 0; i < array.length; i++) {
+            // print out non-zero frequencies - cast to a char
+            if (array[i] > 0) {
 
+                // set variables
+                char c = (char) i;
+                int frequency = array[i];
 
+                // create tree
+                Tree myTree = new Tree();
 
+                // create node for the letter
+                Node myNode = new Node();
+                myNode.letter = c;
+                myNode.smallestLetter = c;
 
+                // set tree attributes
+                myTree.frequency = frequency;
+                myTree.root =  myNode; /* this needs to be a node */;
 
-                //FILL THIS IN:
-
-                //MAKE THE FOREST OF TREES AND ADD THEM TO THE PQ
-
-                //create a new Tree
-                //set the cumulative frequency of that Tree
-                //insert the letter as the root node
-                //add the Tree into the PQ
-
-
-
-
+                PQ.add(myTree);
             }
         }
 
-        while (PQ.size() > 1) { //while there are two or more Trees left in the forest
+        while (PQ.size() > 1) {
+            // while there are two or more Trees left in the forest
 
+            Tree combo = new Tree();
+            Tree firstTree = PQ.poll();
+            Tree secondTree = PQ.poll();
 
+            Node localRoot = new Node();
+            combo.root = localRoot;
+            combo.frequency = firstTree.frequency + secondTree.frequency;
+            localRoot.smallestLetter = firstTree.root.smallestLetter;
+            localRoot.leftChild = firstTree.root;
+            localRoot.rightChild = secondTree.root;
 
-
-            //FILL THIS IN:
-
-            //IMPLEMENT THE HUFFMAN ALGORITHM
-
-            //when you're making the new combined tree, don't forget to create a new root node (or else you'll get a null pointer exception)
-            //make sure to set the smallestLetter of the new combined tree to what it should be
-            //the first tree to come out of PQ goes on the left, the second on the right
-
-
-
+            PQ.add(combo);
         }
 
-        Tree HuffmanTree = PQ.poll(); //now there's only one tree left - get its codes
+        Tree HuffmanTree = PQ.poll();
 
-
-
-
-        //FILL THIS IN:
-
-        //get all the codes for the letters and print them out
-        //call the getCode() method on the HuffmanTree Tree object for each letter in the sentence
-
-
-
-
+        // now there's only one tree left - get its codes
+        for (int l=0; l<sentence.length(); l++){
+            System.out.println(HuffmanTree.getCode(sentence.charAt(l)));
+        }
+        System.out.println();
     }
 }
 
 class Node {
-
-
     public char letter = '@'; //stores letter
     public char smallestLetter = '@';  //a nice idea is to track the smallest letter in the tree in a special variable like this
 
     public Node leftChild; // this node's left child
     public Node rightChild; // this node's right child
+}
 
-
-} // end class Node
-
-
-
-
-
-class Tree implements Comparable < Tree > {
+class Tree implements Comparable<Tree> {
     public Node root; // first node of tree
     public int frequency = 0;
 
-    public Tree() // constructor
-    {
+    // constructor
+    public Tree(){
         root = null;
-    } // no nodes in tree yet
-
-    //the PriorityQueue needs to be able to somehow rank the objects in it
-    //thus, the objects in the PriorityQueue must implement an interface called Comparable
-    //the interface requires you to write a compareTo() method so here it is:
-
+    }
     public int compareTo(Tree object) {
-        if (frequency - object.frequency > 0) { //compare the cumulative frequencies of the tree
+        //compare the cumulative frequencies of the tree
+        if (frequency - object.frequency > 0) {
             return 1;
         } else if (frequency - object.frequency < 0) {
-            return -1; //return 1 or -1 depending on whether these frequencies are bigger or smaller
+            return -1;
+        // return 1 or -1 depending on frequencies being bigger or smaller
         } else {
             // Sort based on letters
             char a = this.root.smallestLetter;
             char b = object.root.smallestLetter;
 
-            if (a > b) {
-                return 1;
-            } else if (a < b) {
-                return -1;
-            }
+            if (a > b) return 1;
+            else if (a < b) return -1;
             return 0;
         }
     }
+    // this variable will track the path to the letter we're looking for
+    String path = "error";
 
-    String path = "error"; //this variable will track the path to the letter we're looking for
-
-    public String getCode(char letter) { //we want the code for this letter
-
-        return this._getCode(letter, this.root, ""); //return the path that results
+    // we want the code for this letter
+    public String getCode(char letter) {
+        // return the path that results
+        return this._getCode(letter, this.root, "");
     }
-
     private String _getCode(char letter, Node current, String path) {
-        if (current == null) {
-            return null;
-        }
-        if (current.letter == letter) {
-            return path;
-        }
+        if (current == null) return null;
+        if (current.letter == letter) return path;
 
         String leftPath = this._getCode(letter, current.leftChild, path + "0");
-        if (leftPath != null) {
-            return leftPath;
-        }
+
+        if (leftPath != null) return leftPath;
 
         String rightPath = this._getCode(letter, current.rightChild, path + "1");
         return rightPath;
     }
-
-} // end class Tree
+}
