b07063644cc5b2b92232893296c8249a042734de
evancn7 <evancn7@gmail.com>
2021-02-27 14:09:59 +0000

    clean code

--- Collatz.java
+++ Collatz.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 class Collatz{
     public static void main(String[] args){
 
-        String sampleInput = "4\n9\n3";
+        String sampleInput = "45\n55\n3";
         Scanner scan = new Scanner(sampleInput);
 
-        int lefter = scan.nextInt();
+        int lower = scan.nextInt();
         int upper = scan.nextInt();
         int index = scan.nextInt();
 
-        int[] array = new int[upper - lefter + 1];
-
+        C[] array = new C[upper - lower + 1];
         int arrayPos = 0; // keep track of the array postion for inserting
 
-        while (lefter <= upper){
-            int comparable = lefter;
+        while (lower <= upper){
+            int comparable = lower;
             int count = 0;
             // comparable is changing in this loop
             while (comparable != 1){
@@ -28,23 +27,26 @@ class Collatz{
                     count++; // one more step is added
                 }
             } // end inner while
-            array[arrayPos] = count; // set current position to count
-            arrayPos++; // move to next postion
-            lefter++;
-        } // end outer while
 
+            C c = new C(lower, count);
+            array[arrayPos] = c;
+            arrayPos++; // move to next postion
+            lower++;
+        } // end outer while
 
         int left = 0;
         int right = array.length - 1;
 
         qSort(array, left, right);
+        bubbleSort(array);
 
-        System.out.println(array[index - 1]);
+        for (C n : array) System.out.println(n);
+
+        // System.out.println(array[index - 1]);
     } // end main
 
-    static void qSort(int a[], int left, int right){
+    static void qSort(C a[], int left, int right){
         if (left < right){
-        /* pi is parting index, arr[pi] is now at right place */
             int p = part(a, left, right);
 
             qSort(a, left, p - 1);
@@ -52,26 +54,49 @@ class Collatz{
         }
     }
 
-    static int part(int a[], int left, int right){
-        // pivot (Element to be placed at right position)
-        int pivot = a[right];
-
-        int i = (left - 1);  // Index of smaller element and indicates the right position of pivot found so far
-
-        for (int j = left; j <= right- 1; j++){
+    static int part(C a[], int left, int right){
+        int pivot = a[right].count;
+        int i = (left - 1);
+        for (int j = left; j <= right - 1; j++){
             // If current element is smaller than the pivot
-            if (a[j] < pivot){
+            if (a[j].count < pivot){
                 i++;    // increment index of smaller element
-
-                int temp = a[i];
+                C temp = a[i];
                 a[i] = a[j];
                 a[j] = temp;
             }
         }
-        int temp = a[i+1];
+        C temp = a[i+1];
         a[i+1] = a[right];
         a[right] = temp;
-
         return (i + 1);
+    }
+    public static C[] bubbleSort(C[] array){
+        for (int j=0; j<array.length; j++){
+            int i = 0;
+            while (i < array.length-1){
+                if (array[i].count  == array[i+1].count){
+                    if (array[i].number > array[i+1].number){
+                        C temp = array[i+1];
+                        array[i+1] = array[i];
+                        array[i] = temp;
+                    }
+                }
+                i++;
+            }
+        }
+        return array;
+    }
+}
+class C{
+    public int number;
+    public int count;
+
+    C(int n, int c){
+        number = n;
+        count = c;
+    }
+    public String toString(){
+        return "n: "+number+" c: "+count;
     }
 }
