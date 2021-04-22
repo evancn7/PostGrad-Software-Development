def4e5423b268e4e4e7a33b117effb577254d1ba
Evan Campion <evancn7@gmail.com>
2021-04-22 12:21:19 +0100

    add basePalindrome & GreatCircleDistance algorithms

---
+++ BasePalindrome.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class BasePalindrome{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    long number = scan.nextLong();
+
+    int total = 0;
+    for (long i = 2; i <=10; i++) {
+      long conv = BPUtils.baseConvertor( number, i );
+      if ( BPUtils.checkPal(conv) ) total++;
+    }
+    System.out.println(total);
+  }
+}
+
+class BPUtils{
+  static long baseConvertor(long number, long base){
+    long r;
+    long power = 0;
+    long result = 0;
+    while (number > 0) {
+      r = number % base;
+      number = number / base;
+      // post-increment power for next loop
+      long factor = (long) Math.pow(10, power++);
+      result += r * factor;
+    }
+    return result;
+  }
+  static boolean checkPalString(long number){
+    String strNumber = Long.toString(number);
+    String revNumber = "";
+    while (number > 0){
+      long digit = number % 10;
+      number = number / 10;
+      revNumber += String.valueOf( digit );
+    }
+    return strNumber.equals(revNumber);
+  }
+  static boolean checkPal(long number){
+    long numberToCheck = number;
+    long revNumber = 0;
+    while (number > 0){
+      long digit = number % 10;
+      number = number / 10;
+      revNumber = revNumber * 10;
+      revNumber += digit;
+    }
+    return numberToCheck == revNumber;
+  }
+}
---
+++ GreatCircleDistance.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class GreatCircleDistance{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    double[] coords = new double[4]; // define array to hold coordinates
+    int pos = 0; // tracks positon for insertion
+    // while new input add coordinate to array but convert it to radians first
+    while( pos < coords.length ) coords[pos++] = degToRad( s.nextDouble() );
+    System.out.println( getDistance( coords ) );
+    // System.out.println( round ( getDistance( coordinates ) ) );
+  }
+  static double getDistance(double[] coords){
+    // coordinates are laid out as:  lat 1 , long 1 , lat 2 , long 2
+    double deltaLong = coords[1] - coords[3];
+    double theta =
+    Math.acos(
+    ( Math.sin(coords[0]) * Math.sin(coords[2]) ) +
+    (( Math.cos(coords[0]) * Math.cos(coords[2]) ) * Math.cos( deltaLong ) )
+     );
+     double distance = 6371 * theta;
+     return distance;
+  }
+  static double degToRad(double coordinate){ // convert the cords to radians
+    return (coordinate * Math.PI) / 180;
+  }
+  static int round( double target ){
+    int number = (int) target;  // convert double distance to integer
+    int rem = number % 100;
+    if ( rem < 50 ) return number - rem;
+    else return number + (100 - rem);
+  }
+}
